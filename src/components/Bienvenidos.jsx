a6d01ff10d716a0acc54ebeeafc07bc6ad33a3e2
deborolon <debora.nrolon@gmail.com>
2024-11-30 13:24:40 -0300

    First commit

---
+++ src/components/Bienvenidos.jsx
@@ -0,0 +1,9 @@
+
+function Bienvenidos({saludo}) {
+    
+    return (
+        <div>{saludo}</div>
+    )
+}
+
+export default Bienvenidos
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+//Importaciones de dependencias, librerias, modulos
+
+//Componente de React (componentes funcion)
+function Welcome() {
+    
+    //En este bloque podemos tener la lógica
+
+    //Dentro del return va la respuesta. Eso que quiero mostrar o eso que necesita otro componente.
+    return (
+        <div>"Welcome!"</div>
+    )
+}
+
+export default Welcome
